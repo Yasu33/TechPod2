62536e0151e1b9d1319fecbc02494d9f6b1fb60d
Yasu33 <m33love.ya.ko@gmail.com>
2019-02-09 18:47:00 +0900

    Initial Commit

---
+++ TechPod2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechPod2
+//
+//  Created by Yasuko Namikawa on 2019/02/09.
+//  Copyright © 2019年 Yasuko Namikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
